2ed02405dd592b73e4bb7be66b3585c672ab79e0
Lucenap <141670475+Lucenap@users.noreply.github.com>
2025-01-29 20:15:06 -0300

    Commit para confirmar aalterações

--- obj/Debug/net9.0/RpgRecommendation.AssemblyInfo.cs
+++ obj/Debug/net9.0/RpgRecommendation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgRecommendation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55907dc23a26da5076e0e3a5ec66d1237c3cc8d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgRecommendation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgRecommendation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
